46a6c5be023c52ad6dd0a6e39cfc013dc63127da
Michał Budziak <michal.mariusz.b@gmail.com>
2020-09-12 15:52:27 +0200

    Change structure

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module pestka
+module github.com/digital-radio/pestka
 
 go 1.14
 
 require (
 	github.com/MonkeyBuisness/golang-iwlist v0.0.0-20190208105021-719e9e53bbf0
 	github.com/gorilla/mux v1.7.4
+	github.com/stretchr/testify v1.6.1
 )
--- src/main.go
+++ src/main.go
@@ -1,66 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/digital-radio/pestka/src/app"
+	"github.com/digital-radio/pestka/src/container"
 	"log"
 	"net/http"
-
-	"github.com/MonkeyBuisness/golang-iwlist"
-	"github.com/gorilla/mux"
 )
 
-var interfaceName = "wlan0"
-
-func response(w http.ResponseWriter, data interface{}, status int) {
-	w.Header().Set("Content-Type", "application/json")
-
-	body, err := json.Marshal(data)
-
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error": "error marshalling data"}`))
-		return
-	}
-
-	w.WriteHeader(status)
-	w.Write(body)
-}
-
-func handleError(w http.ResponseWriter, err error) {
-	log.Println(err)
-	var data = map[string]string{
-		"message": err.Error(),
-	}
-	response(w, data, http.StatusInternalServerError)
-}
-
-func networkCollection(w http.ResponseWriter, r *http.Request) {
-	cells, err := wlist.Scan(interfaceName)
-	if err != nil {
-		handleError(w, err)
-		return
-	}
-
-	response(w, cells, http.StatusOK)
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	var data = map[string]string{
-		"message": "not found",
-	}
-	response(w, data, http.StatusOK)
-}
-
 func main() {
-	var r = mux.NewRouter()
-	r.HandleFunc("/networks", networkCollection).Methods(http.MethodGet)
-	r.HandleFunc("/", notFound)
-
+	var c = container.New()
+	var a = app.New(c)
+	var router = a.CreateRouter()
 	var port = 8080
 	var addr = fmt.Sprint(":", port)
 
 	log.Println("Listening on port:", port)
-	log.Fatal(http.ListenAndServe(addr, r))
+	log.Fatal(http.ListenAndServe(addr, router))
 }
