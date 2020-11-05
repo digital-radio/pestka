a01fb5486b05611382e2e9238e8062cd40803e1a
Kingua <kingaaa.b@gmail.com>
2020-11-05 19:12:32 +0100

    Change client namespace

--- poc_dbus/poc_dbus_client/client.go
+++ poc_dbus/poc_dbus_client/client.go
@@ -13,13 +13,13 @@ func main() {
 	}
 
 	// func (conn *Conn) Object(dest string, path ObjectPath) *Object
-	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+	obj := conn.Object("pl.digital-radio", "/malina")
 
 	// Interface from the specification:
 	// UINT32 org.freedesktop.Notifications.Notify (STRING app_name, UINT32 replaces_id, STRING app_icon, STRING summary, STRING body, ARRAY actions, DICT hints, INT32 expire_timeout);
 
 	// func (o *Object) Call(method string, flags Flags, args ...interface{}) *Call
-	call := obj.Call("org.freedesktop.Notifications.Notify", 0, "c¼h", uint32(0), "", "Hallo Chaostreff!", "Ich begrüße euch herzlich zu meiner c¼h!", []string{}, map[string]dbus.Variant{}, int32(1000))
+	call := obj.Call("pl.digital-radio.Notify", 0, "c¼h", uint32(0), "", "Hallo Chaostreff!", "Ich begrüße euch herzlich zu meiner c¼h!", []string{}, map[string]dbus.Variant{}, int32(1000))
 	if call.Err != nil {
 		panic(call.Err)
 	}
