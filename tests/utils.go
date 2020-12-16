22bb8e46f566a9626153d4a36ea7ae8942b1da15
Kingua <kingaaa.b@gmail.com>
2020-12-16 23:43:21 +0100

    Add comments to test. Remove old FIXME.

--- tests/utils.go
+++ tests/utils.go
@@ -2,31 +2,34 @@ package tests
 
 import (
 	"encoding/json"
-	"github.com/digital-radio/pestka/src/app"
-	"github.com/digital-radio/pestka/src/container"
 	"io"
 	"log"
 	"net/http/httptest"
+
+	"github.com/digital-radio/pestka/src/app"
+	"github.com/digital-radio/pestka/src/container"
 )
 
+//TestApp is a struct simulating app during tests.
 type TestApp struct {
 	Container container.Container
 }
 
-//New allows to create a new Container struct outside of package container.
+//CreateTestApp allows to create an app just for testing.
 func CreateTestApp() TestApp {
 	return TestApp{container.New()}
 }
 
+//MakeRequest allows to simulate http server during tests.
 func (a *TestApp) MakeRequest(method, target string, body io.Reader) *httptest.ResponseRecorder {
 	var application = app.New(a.Container)
 	var router = application.CreateRouter()
-	//FIXME Co to robi ?
 	var w = httptest.NewRecorder()
 	router.ServeHTTP(w, httptest.NewRequest(method, target, body))
 	return w
 }
 
+//MarshallJson helper method to marshall json.
 func MarshallJson(input interface{}) string {
 	bytes, err := json.Marshal(input)
 
@@ -38,6 +41,7 @@ func MarshallJson(input interface{}) string {
 	return string(bytes)
 }
 
+//JsonRemarshal helper method to re-marshall json.
 func JsonRemarshal(bytes []byte) []byte {
 	var ifce interface{}
 	err := json.Unmarshal(bytes, &ifce)
